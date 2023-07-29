0baa9fc9c94344dda7773881967474aee7a78cc1
EnGentech <engen.inyang@gmail.com>
2023-07-29 15:24:37 -0700

    modified 

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -89,7 +89,7 @@ class FileStorage:
                 clas = objs.split('.')
                 if clas[0] == cls.__name__:
                     count += 1
-            return count   
+            return count
         else:
             for obj in self.__objects:
                 count += 1
