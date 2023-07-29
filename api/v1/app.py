6b364d1096e84d76a31c214fd2df8d404fec9711
EnGentech <engen.inyang@gmail.com>
2023-07-29 16:51:43 -0700

    pushed 

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,13 +5,28 @@ Create flask web application
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
-app = Flask('__name__')
+host_use = getenv('HBNB_API_HOST')
+port_use = getenv('HBNB_API_PORT')
+
+if host_use:
+    hst = host_use
+else:
+    hst = '0.0.0.0'
+
+if port_use:
+    pt = port_use
+else:
+    pt = 5000
+
+app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def storage_close(self):
-    return storage.close()
+def storage_close():
+    """a teardown context to close the session"""
+    storage.close()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=hst, port=pt, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The index folder package"""
+
+#from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route('/status')
+def status():
+    """Return the status of the page"""
+    return 'OK'
