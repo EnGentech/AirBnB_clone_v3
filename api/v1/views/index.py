7a99edf2e60222b2e70f82a35c11c5e022c8d0ac
EnGentech <engen.inyang@gmail.com>
2023-07-29 18:23:18 -0700

    continue sir favour 

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,13 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.place import Place
+from models.user import User
+from models.state import State
+from models.review import Review
+from models.city import City
 
 
 @app_views.route('/status')
@@ -12,3 +19,22 @@ def status():
             'status': 'OK'
             }
     return jsonify(status)
+
+@app_views.route('/api/v1/stats')
+def count():
+    """Return the number of each objects in database"""
+    objects = {
+            'amenities': Amenity,
+            'cities': City,
+            'places': Place,
+            'reviews': Review,
+            'states': States,
+            'users': User
+            }   
+    my_dict = {}
+    for key, value in objects.items():
+        new_dict = {
+                key: storage.count(value)
+                }
+        my_dict.update(new_dict)
+    return jsonify(my_dict)
