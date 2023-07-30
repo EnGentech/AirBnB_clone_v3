214126fae341fa11db610ec5c0e4c592bedf53a0
EnGentech <engen.inyang@gmail.com>
2023-07-29 17:20:06 -0700

    made our first route 

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def storage_close():
+def storage_close(self):
     """a teardown context to close the session"""
     storage.close()
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """The index folder package"""
 
-#from flask import jsonify
+from flask import jsonify
 from api.v1.views import app_views
 
 @app_views.route('/status')
 def status():
     """Return the status of the page"""
-    return 'OK'
+    status = {
+            'status': 'OK'
+            }
+    return jsonify(status)
